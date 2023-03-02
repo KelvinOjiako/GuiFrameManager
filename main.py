db95559137a4110cb2c65181f395b6ab1dddc985
kelvin Ojiako <51576899+KelvinOjiako@users.noreply.github.com>
2023-03-02 16:40:48 -0500

    PushButtons that iterate UI elements

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from FrameManager.GuiTestManager import experimental_main
-from guizero import App
+from FrameManager.GuiTestManager import experimental_main, move_to_previous_widget, move_to_next_widget
+from guizero import App, PushButton
 
 
 def sample_gui_test():
     app = App(title="Preserve the Nerve Sample Project")
     manager = experimental_main(app)
+    manager.activate_current_element()
+    previous_button = PushButton(app, text="prev", command=move_to_previous_widget, args=[manager])
+    next_button = PushButton(app, text="next", command=move_to_next_widget, args=[manager])
+    app.display()
 
 
 if __name__ == '__main__':
